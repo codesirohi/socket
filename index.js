fb8bf81c95aa265cbc4ca80115dcbe9c65f56794
codesirohi <siddhantshubham23@gmail.com>
2022-03-09 02:40:12 +0530

    until step 4

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+import { Dictionary } from 'dictionaryjs';
+
+
+// book keeping of connected devices
+let activeConnectionDict = new Dictionary();
+
+
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-//Whenever someone connnects this gets executed
+//Whenever someone connnects this gets executed  
 io.on("connection", function(socket) {
-  console.log( " user Connected id: " + socket.id);
+  // console.log( " user Connected id: " + socket.id);
+  console.log(`client with id : ${socket.id} connected to apigateway`);
+
+  activeConnectionDict.set(socket.id, socket);
+  console.log("number of client connected : ", activeConnectionDict.length);
+
+  
 
   //Whenever someone disconnects this one gets executed
   socket.on("disconnect", function() {
-    console.log(" user disconnected ");
+    console.log("Client disconnected: ", socket.id);
   });
 });
 
